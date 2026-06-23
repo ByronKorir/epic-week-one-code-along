40f35287b8802cfbe36875c49e947a2902f6c4bf
Byron Korir <byronkorir2@gmail.com>
2026-06-23 10:51:26 +0300

    added intro to javascript code along

---
+++ week2/day1/index.js
@@ -0,0 +1,117 @@
+// console.log("Hello world, Js")
+// console.log("line 2")
+
+// variable
+let grade = 12
+// console.log(grade)
+
+grade = 20 // how to update 
+// console.log(grade)
+
+
+const myName="Byron"
+const PI = 3.142
+
+// PI = 20
+// console.log(PI)
+
+// const myName = "Byron"
+// const my-name = "Byron" => error
+// const 1name = ""
+const my_name = "Byron"
+const username123 = "insider"
+
+
+// DATA TYPES
+// string 
+let name = "John"
+let county = 'Kenya'
+let message ="Hello world, introduction to js"
+// console.log(typeof(name),name)
+//number
+let age = 10
+// console.log(age, typeof(age))
+
+
+let pi = 3.142
+// console.log(pi, typeof(pi))
+
+//boolean = true or false
+let isLogin = true
+// console.log(isLogin, typeof(isLogin))
+isLogin = false
+// console.log(isLogin, typeof(isLogin))
+
+//null
+let isUser = null
+// console.log(isUser, typeof(isUser))
+
+//undefined
+let animal = "dog";
+// console.log(typeof(animal))
+
+// ARRAY 
+const fruits = ["oranges", "mangoes",]
+
+// OBJECT
+const student = {
+    id:1,
+    name:"Byron",
+    email:"example@gmail.com",
+
+}
+
+// OPERATORS
+let a = 10
+let b = 3 
+// arithmetic 
+// console.log(a + b, "add", "cart total");    // 13 (Addition)
+// console.log(a - b, "sub");    // 7 (Subtraction)
+// console.log(a * b, "mult");    // 30 (Multiplication)
+// console.log(a / b, "div");    // 3.333... (Division)
+// console.log(a % b, "modulus");    // 1 (Modulus - Remainder) 
+// console.log(a ** b, "power");   // 1000 (Exponentiation)  10 *10 * 10 * 10 
+
+ 
+// COMPARISON
+let c = 5
+let d = "5"
+// equal
+// console.log(c == d)
+// console.log(c === d)
+// console.log(c != d) //false
+// console.log(c !== d) //true
+
+// relational
+// >, < ,>=, <=
+// console.log(10 > 5);          // true
+// console.log(10 < 5);          // false
+// console.log(10 >= 10);        // true
+// console.log(10 <= 5);         // false
+
+// logical
+// and(&) , or(|), not(!)
+let myAge = 25 
+let hasLicense = true
+// and
+// console.log(myAge >= 18 && hasLicense);  // true
+
+// or
+let hasPassport = false;
+let hasID = true;
+
+// console.log(hasPassport || hasID);     // true (One true)
+// console.log(hasPassport || hasID);     // false (Both false)
+
+let isLoggedIn = true;
+
+// console.log(!isLoggedIn);              // false
+// console.log(!!isLoggedIn);             // true (Double negation)
+
+
+// OUTPUT 
+// alert("myAge")
+
+// INPUT
+// let firstName = prompt("enter name")
+// console.log(firstName)
